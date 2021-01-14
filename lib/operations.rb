6c2f95fd1257c4af2a3f470d0963bce903fe470a
Stephanie La Raja <stephanielaraja@gmail.com>
2021-01-14 16:09:30 -0500

    commit tree dee3d9962740bb6e9ae7c8914201b8af19737c87

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,10 @@
 def unsafe?(speed)
-
-end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
-
+    if ((speed < 40) || (speed > 60))
+      return true
+    else
+      return false
+    end
+  end
+  def not_safe?(speed)
+      (speed < 40 || speed > 60) ? true : false
+  end
